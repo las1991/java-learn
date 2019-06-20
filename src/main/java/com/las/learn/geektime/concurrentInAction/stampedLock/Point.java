81f947dcfaff20f0793a72972266e60dc20fe9b4
las <las@sengled.com>
2019-06-20 17:42:56 +0800

    stamped lock example

--- src/main/java/com/las/learn/geektime/concurrentInAction/stampedLock/Point.java
+++ src/main/java/com/las/learn/geektime/concurrentInAction/stampedLock/Point.java
@@ -16,6 +16,7 @@ public class Point {
         this.y = y;
     }
 
+    //乐观读 升级 悲观读
     double distanceFromOrigin() {
 
         long stamp = stampedLock.tryOptimisticRead();
@@ -34,4 +35,26 @@ public class Point {
 
         return Math.sqrt(curX * curX + curY * curY);
     }
+
+    //读升级写
+    void moveIfAtOrigin(int newX, int newY) {
+        long stamp = stampedLock.readLock();
+        try {
+            while (x == 0 && y == 0) {
+                long ws = stampedLock.tryConvertToWriteLock(stamp);
+                if (ws != 0) {
+                    x = newX;
+                    y = newY;
+                    stamp = ws;
+                    break;
+                } else {
+                    stampedLock.unlockRead(stamp);
+                    stamp = stampedLock.writeLock();
+                }
+            }
+        } finally {
+            stampedLock.unlock(stamp);
+        }
+    }
+
 }
